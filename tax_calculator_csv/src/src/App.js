2656e6850d3070c59e0f3bbbacc6b1b60bc4679b
Devashishsaurabh <onlyvishwasindia@gmail.com>
2022-12-30 12:13:40 +0530

    csv

---
+++ tax_calculator_csv/src/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CsvCalculator from './Components/CsvCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <CsvCalculator/>
+    </div>
+  );
+}
+
+export default App;
