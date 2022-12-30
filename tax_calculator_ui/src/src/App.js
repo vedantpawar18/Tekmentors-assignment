87498682ab69126ad18bd41d86f1dc4455f72838
Devashishsaurabh <onlyvishwasindia@gmail.com>
2022-12-30 11:41:36 +0530

    update

---
+++ tax_calculator_ui/src/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CalculatorUi from './components/CalculatorUi';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculatorUi/>
+    </div>
+  );
+}
+
+export default App;
