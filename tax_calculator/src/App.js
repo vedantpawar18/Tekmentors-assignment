279d464df84a75694f7a13a8ead609342ceee547
Devashishsaurabh <onlyvishwasindia@gmail.com>
2022-12-31 20:37:03 +0530

    completed

--- tax_calculator/src/App.js
+++ tax_calculator/src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import CalculatorUi from './components/CalculatorUi';
-import CalculatorCsv from './components/CalculatorCsv'
-import { Heading } from '@chakra-ui/react';
+import "./App.css";
+import CalculatorUi from "./components/CalculatorUi";
+import CalculatorCsv from "./components/CalculatorCsv";
+import { Heading } from "@chakra-ui/react";
 function App() {
   return (
     <div className="App">
       <Heading>Problem 1</Heading>
-      <CalculatorCsv/>
-      <br/>
-      <br/>
-      <hr/>
-      <br/>
+      <CalculatorCsv />
+      <br />
+      <br />
+      <hr />
+      <br />
       <Heading>Problem 2</Heading>
-      <CalculatorUi/>
+      <CalculatorUi />
     </div>
   );
 }
